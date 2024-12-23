11f97297e4ae0c57a250a0a6c1784fb1d1e41114
Calvin <lapp.cd@gmail.com>
2024-12-22 22:44:37 -0500

    Tree sitter
    Also re-installed the theme cuz it was messing with stuff??

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,9 @@
 require("config.lazy")
 
-local opt = vim.opt
-opt.shiftwidth = 4
-opt.number = true
-opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>") -- Execute current file
 vim.keymap.set("n", "<space>x", ":.lua<CR>") -- Execute current line
