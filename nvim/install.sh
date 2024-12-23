ea8900f84aa03e48bc5bece0785480f5a0f1f04c
Calvin <lapp.cd@gmail.com>
2024-12-22 21:09:49 -0500

    Modify neovim install point

--- nvim/install.sh
+++ nvim/install.sh
@@ -11,7 +11,7 @@ pushd neovim
 rm -r build/
 
 # Build Neovim with the desired installation prefix
-make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX="$HOME/.dotfiles/nvim"
+make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX="$HOME/.config/nvim/build"
 
 # Install Neovim to the specified location
 make install 
@@ -20,10 +20,10 @@ make install
 popd
 
 # Update PATH to include the bin directory of the custom install location
-if [[ ":$PATH:" == *":$HOME/bin:"* ]]; then
+if [[ ":$PATH:" == *":$HOME/nivm/build/bin:"* ]]; then
   echo "nvim already in path"
 else
-  export PATH="$HOME/bin:$PATH"
+  export PATH="$HOME/nvim/build/bin:$PATH"
   echo "nvim has been added to path"
 fi
 
