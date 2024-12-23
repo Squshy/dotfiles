2228ae3e6d8864a5299175bd4cf8926117a81e0b
Calvin <lapp.cd@gmail.com>
2024-12-22 22:08:12 -0500

    Start of option configs

---
+++ nvim/after/ftplugin/lua.lua
@@ -0,0 +1,9 @@
+-- First part of file should be file type
+-- ftplugin is file type plugins
+-- Put in after dir to run _after_ default config goes
+local set = vim.opt_local
+
+set.shiftwidth = 2
+set.number = true
+set.relativenumber = true
+
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,10 @@
 require("config.lazy")
+
+local opt = vim.opt
+opt.shiftwidth = 4
+opt.number = true
+opt.relativenumber = true
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>") -- Execute current file
+vim.keymap.set("n", "<space>x", ":.lua<CR>") -- Execute current line
+vim.keymap.set("v", "<space>x", ":lua<CR>") -- Select some lines, execute them
