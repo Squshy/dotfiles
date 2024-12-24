5535f1faabcea016ec5d8736d1364d9c3c225103
Calvin <lapp.cd@gmail.com>
2024-12-24 00:08:59 -0500

    Remove old comments

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,25 +1,17 @@
 # Install build dependencies on Ubuntu
 sudo apt-get install ninja-build gettext cmake unzip curl build-essential
 
-# Clone Neovim repository
 git clone https://github.com/neovim/neovim
 
-# Navigate into the Neovim directory
 pushd neovim
 
-# Remove the existing build directory to clear CMake cache
 rm -r build/
 
-# Build Neovim with the desired installation prefix
 make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX="$HOME/.config/nvim/build"
-
-# Install Neovim to the specified location
 make install 
 
-# Return to the previous directory
 popd
 
-# Update PATH to include the bin directory of the custom install location
 if [[ ":$PATH:" == *":$HOME/nivm/build/bin:"* ]]; then
   echo "nvim already in path"
 else
