2656b93fe35bd9c1738004621d3dfd85ef792986
Calvin <lapp.cd@gmail.com>
2024-12-28 00:58:22 -0500

    Add stow script

---
+++ stow.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# stow this entire dir except for these guys
+# stow this way so I have have directories, instead of loose files
+stow $HOME/.config -t ~/ --ignore="fonts" --ignore="nvm" --ignore="stow.*" --ignore="README.md" 
