eb75fb59d0aefe0e0507c2ffe4483c806cd59422
Calvin <lapp.cd@gmail.com>
2024-12-28 14:27:11 -0500

    Remove return true

--- utils.sh
+++ utils.sh
@@ -6,11 +6,9 @@ add_to_path() {
 
     if [[ ":$PATH:" == *":$NEW_PATH:"* ]]; then
       echo "$TITLE already in path at $NEW_PATH"
-      return false
     fi
 
     export PATH="$NEW_PATH:$PATH"
     echo "$TITLE has been added to path at $NEW_PATH. You will want to persist this."
-    return true
 }
 
