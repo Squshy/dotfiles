03ec2ca66abd57a6445cf075dd62c35cc2eecb01
Calvin <lapp.cd@gmail.com>
2024-12-28 15:00:32 -0500

    Modify nvim install

--- nvim/install.sh
+++ nvim/install.sh
@@ -4,15 +4,16 @@ source $HOME/.config/utils.sh
 # Install build dependencies on Ubuntu
 sudo apt-get install ninja-build gettext cmake unzip curl build-essential
 
+pushd $HOME/.config/nvim
 git clone https://github.com/neovim/neovim
 
-pushd neovim
-
 rm -r build/
+pushd $HOME/.config/nvim/neovim
 
 make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX="$HOME/.config/nvim/build"
 make install 
 
+popd
 popd
 
 add_to_path "nvim" "$HOME/nivm/build/bin"
